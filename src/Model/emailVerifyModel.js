d50b316de75a886dc658ea08427d01225963e8e9
horleng <ly.horleng.1221@rupp.edu.kh>
2023-02-20 22:30:15 +0700

    updated4

--- src/Model/emailVerifyModel.js
+++ src/Model/emailVerifyModel.js
@@ -4,6 +4,6 @@ const sendCode = mongoose.Schema({
     email:{type:String,required:true},
     otp:{type:Number,required:true},
     createdAt:{type:Date,default:Date.now,expires:"1h"}
-});
+},{timestamps:true});
 
 module.exports = mongoose.model('Email_Verify',sendCode);
